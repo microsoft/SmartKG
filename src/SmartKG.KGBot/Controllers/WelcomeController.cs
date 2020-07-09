2f7f75fcb5cf3f90e0cd6ec5585fa91a33ea2ae7
Julia Li <ye.julia.li@outlook.com>
2020-07-09 14:29:31 +0800

    move values to welcome API

---
+++ src/SmartKG.KGBot/Controllers/WelcomeController.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartKG.KGBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : Controller
+    {
+        // GET api/welcome
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Welcome to SmarKG" };
+        }
+
+        // GET api/welcome/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "SmartKG";
+        }       
+    }
+}
