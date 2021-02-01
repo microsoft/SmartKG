3609da5fafcb5c3d016645cef0d434f017af35d5
Julia Li <jull@microsoft.com>
2021-02-01 13:38:54 +0800

    add new config files

--- SmartKGUI/public/config.js
+++ SmartKGUI/public/config.js
@@ -1,4 +1,3 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-// var urlapi = "https://localhost:5001"
-var urlapi = "http://104.214.140.219:8082"
+var urlapi = "https://localhost:5001"
