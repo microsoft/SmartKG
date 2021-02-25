d814cf23cbdecb5e8df3773ff6f06048f1adacc1
chenqingot <14864733@qq.com>
2021-02-25 16:40:13 +0800

    add agent param for sensitive page

--- SmartKGUI/public/config.js
+++ SmartKGUI/public/config.js
@@ -1,3 +1,4 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-var urlapi = "https://localhost:5001"
+// var urlapi = "https://localhost:5001"
+var urlapi = "http://52.184.36.94:8082"
